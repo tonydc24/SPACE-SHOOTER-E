a01fdd489fa9378af870fccdbae7c7432a3c1179
tonydc24 <124815616+tonydc24@users.noreply.github.com>
2024-07-08 12:19:05 -0600

    Fix
    Test

--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -221,21 +221,17 @@ public class SpaceshipController : MonoBehaviour
        
         yield return new WaitForSeconds(dieWaitTime);
 
-
         //MOVE TO LEVELMANAGER AS RELOAD() ==> 10pts Ready!
         LevelManager levelManager = FindObjectOfType<LevelManager>();
 
         if (UIController.Instance.HasLives() == false)
         {
-            levelManager.Reload();
+            levelManager.NextLevel();
         }
         else 
         {
-            
-            levelManager.NextLevel();
+            levelManager.Reload();
         }
-        
-
         //GO TO GAMEOVER WHEN HasLives IS FALSE => 10pts Ready?
     }
 }
